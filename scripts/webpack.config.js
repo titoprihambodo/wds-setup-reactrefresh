03f138420dc89373bfceba6a3fed9c091264ce71
titoprihambodo <prihambodo.tito@gmail.com>
2024-07-02 12:24:48 +0700

    wds starter

---
+++ scripts/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const WebpackAssetsManifest = require('webpack-assets-manifest');
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const ReactRefreshTypeScript = require('react-refresh-typescript');
+
+const resolvePathFromRoot = (relativePath) => 
+  path.resolve(__dirname, '../', relativePath);
+
+module.exports = {
+  mode: 'development',
+  entry: resolvePathFromRoot('src/client/index.tsx'), 
+  output: {
+    filename: 'assets/js/bundle-[name]-[fullhash].js',
+    path: resolvePathFromRoot('dist'),
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new ReactRefreshPlugin(),
+    new WebpackAssetsManifest({
+      output: 'assets-manifest.json',
+      entrypoints: true,
+      integrity: true,
+    }),
+  ].filter(Boolean),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_module/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            getCustomTransformers: () => ({
+              before: [ReactRefreshTypeScript()].filter(Boolean),
+            }),
+            transpileOnly: true,
+          },
+        },
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx',],
+  },
+};
